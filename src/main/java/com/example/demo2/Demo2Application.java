54f161896d5057a2095fb75d7981eed74ac45f31
Phoebe Ho <phoebe.ho@shoalter.com>
2023-05-18 17:51:16 +0800

    feat: 將專案改成Command Line Runner Application，並在執行完複製 table 工作後關閉 Application

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -1,13 +1,23 @@
 package com.example.demo2;
 
+import com.example.demo2.controller.DataReplicationController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo2Application {
+@RequiredArgsConstructor
+public class Demo2Application implements CommandLineRunner {
+	private final DataReplicationController dataReplicationController;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		dataReplicationController.replicateWorkTime();
+		System.exit(0);
+	}
 }
